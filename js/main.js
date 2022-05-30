e9475816a2c2afe3c0e07675882c860dba9832a0
Zsolt <horvathzs@gmail.com>
2022-05-30 08:25:09 +0200

    otodik nap - parameteres fv-ek

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
 
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    showSumPrice(price, amountNumber);
+}
+
+function showSumPrice(price = 1000, amountNumber = 1) {
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
 
     if (amountNumber > 10) {
+        amountInput.value = 10;
+        showAmount.innerHTML = amountInput.value * price;
         alert("Nem vásárolhatsz többet mint 10 hambi.");
     } else if
         (amountNumber < 1) {
+        amountInput.value = 1;
+        showAmount.innerHTML = amountInput.value * price;
         alert("Legalább egy hambit kell vásárolni.");
     } else {
         let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
+
 }
