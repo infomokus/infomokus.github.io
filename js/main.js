0b04022372e37c12d1c1296b40f45fbbabda24a4
Zsolt <horvathzs@gmail.com>
2022-05-30 17:02:16 +0200

    7. nap

--- js/main.js
+++ js/main.js
@@ -24,5 +24,39 @@ function showSumPrice(price = 1000, amountNumber = 1) {
         let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
-
 }
+
+
+function betoltes() {
+    document.querySelector("span#mainap").innerHTML = nap;
+}
+
+ let napok = [
+    [id="0", value="vasárnap"],
+    [id="1", value="hétfő"],
+    [id="2", value="kedd"],
+    [id="3", value="szerda"],
+    [id="4", value="csütörtök"],
+    [id="5", value="péntek"],
+    [id="6", value="szombat"],
+];
+
+
+// let napok = ["vasárnap","hétfő", "kedd", "szerda", "csutortok", "pentek", "szombat"]
+let selectBox = document.querySelector("select");
+for (let i = 0; i < napok.length; i++) {
+    selectBox.options.add(new Option(napok[i][1]))
+}
+
+let d = new Date();
+let nap = napok[d.getDay()][1];
+let hanyadiknap = napok[d.getDay()];
+let homersekletek = [11, 13, 15, 17, 19, 21, 23];
+
+document.querySelector('span#homerseklet').innerHTML = homersekletek[d.getDay()];
+
+function szamol(){
+console.log("szamolok")
+document.querySelector('span#homerseklet').innerHTML = "listelemnek megfelelő c";
+};
+
